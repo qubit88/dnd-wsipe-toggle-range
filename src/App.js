00a9bb1522f9a332db6040d8eb5b0989ed4fbeac
qubit88 <qubertec@gmail.com>
2019-08-04 16:38:50 +0300

    add on move handler to app to insert dragged item

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import data from "./data";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      data
+    };
+  }
+
+  OnMove = (id, prevId, after) => {
+    let movedItem;
+    const newList = this.state.data.filter(item => {
+      if(item.id === id) {
+        movedItem = item;
+        return;
+      }
+
+      return item;
+      
+    } );
+
+    let prevIndex = newList.findIndex(item => item.id === prevIndex);
+
+    const newIndex = after ? prevIndex  : prevIndex + 1;
+
+    newList.splice(newIndex, 0, movedItem);
+  }
+
+  
+
+  render() {
+    const type1 = this.state.data.filter(item => item.type === "one");
+    const type2 = this.state.data.filter(item => item.type === "two");
+
+    const Item = ({item, isActive}) => (<ListItem item={item} isActive={isActive} />)
+
+    return (<div className="App"> 
+          <h1>One</h1>
+
+          <DraggableList data={type1} DraggableItem={Item} onMove={} ></DraggableList>
+          
+          <h1>Two</h1>
+
+          <DraggableList data={type2} DraggableItem={Item} onMove={} ></DraggableList>
+    </div>);
+  }
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,56 @@
+export default [
+  {
+    id: 0,
+    name: "JavaScript",
+    favorite: false,
+    type: "one"
+  },
+  {
+    id: 1,
+    name: "TypeScript",
+    favorite: false,
+    type: "two"
+  },
+  {
+    id: 2,
+    name: "Java",
+    favorite: false,
+    type: "one"
+  },
+  {
+    id: 3,
+    name: "C#",
+    favorite: false,
+    type: "one"
+  },
+  {
+    id: 4,
+    name: "C++",
+    favorite: false,
+    type: "one"
+  },
+  {
+    id: 5,
+    name: "Python",
+    favorite: false,
+    type: "two"
+  },
+  {
+    id: 6,
+    name: "Swift",
+    favorite: false,
+    type: "two"
+  },
+  {
+    id: 7,
+    name: "Dart",
+    favorite: false,
+    type: "two"
+  },
+  {
+    id: 8,
+    name: "Ruby",
+    favorite: false,
+    type: "two"
+  }
+];
